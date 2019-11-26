bfeb299a51bdba2e7c32764cf6c314be9600a067
kib4885 <kib4885@gmail.com>
2019-11-26 17:32:08 -0500

    kb11261

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -5,7 +5,7 @@ function Jumbotron({ children }) {
   return (
     <div
       style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-      className="jumbotron" id="text"
+      className="jumbotron"
     >
       {children}
     </div>
